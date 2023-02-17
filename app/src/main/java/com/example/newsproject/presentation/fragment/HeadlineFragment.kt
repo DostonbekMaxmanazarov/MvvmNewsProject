94aeff51bc76eafabfbb84bb39d500e8d47c030d
Dostonjon-98 <mahmanazarovdoston98@gmail.com>
2023-02-17 17:59:48 +0500

    add headline news fragment

---
+++ app/src/main/java/com/example/newsproject/presentation/fragment/HeadlineFragment.kt
@@ -0,0 +1,6 @@
+package com.example.newsproject.presentation.fragment
+
+import androidx.fragment.app.Fragment
+import com.example.newsproject.R
+
+class HeadlineFragment:Fragment(R.layout.fragment_headlines) {}
